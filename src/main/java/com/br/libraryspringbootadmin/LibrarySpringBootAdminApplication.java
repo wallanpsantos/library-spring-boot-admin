ca683324027ef754167b3c40743291e7ba12d794
wallanpsantos <wallanpereira09@gmail.com>
2022-07-27 09:36:36 -0300

    Iniciando projeto

---
+++ src/main/java/com/br/libraryspringbootadmin/LibrarySpringBootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.br.libraryspringbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySpringBootAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/libraryspringbootadmin/LibrarySpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.libraryspringbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
